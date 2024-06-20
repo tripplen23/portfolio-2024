96155e41660faac0d9b867bd392a66e1e5da536a
Nguyen Duc Binh <tripplen63@gmail.com>
2024-06-20 20:41:50 +0300

    Update config for deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const path = require("path");
+
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
+};
